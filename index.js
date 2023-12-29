eeacab3442b6fa682b63a58de0712dc066d74209
SyM0Re <smrrehansmr@gmail.com>
2023-12-29 21:14:39 +0530

    First form commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let a = document.getElementById("header");
+a.style.color = "red"
+let b = document.getElementById("main-heading");
+b.style.color = "blue"
+let c = document.getElementsByClassName("fruits");
+c.style.color = "yello";
+let div = document.getElementById("thanks");
+div.style.backgroundColor = "grey";
