79422f8aace4e4c35d802e6fa418d9743a92774a
SyM0Re <smrrehansmr@gmail.com>
2023-12-30 11:35:54 +0530

    getElementsByClassName

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-let a = document.getElementById("header");
-a.style.color = "red"
-let b = document.getElementById("main-heading");
-b.style.color = "blue"
-let c = document.getElementsByClassName("fruits");
-c.style.color = "yello";
-let div = document.getElementById("thanks");
-div.style.backgroundColor = "grey";
+let a = document.getElementsByClassName("fruit");
+a.style.fontWeight = "bold";
