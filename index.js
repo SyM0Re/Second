9679d99d13951d28342c8205112585f60860a54f
SyM0Re <smrrehansmr@gmail.com>
2024-01-04 19:53:22 +0530

    Edit_Functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let c = 0;
 let ul = document.querySelector("ul");
-// creating an object and storing in local storage
+
+// creating an object
 function handleFormSubmit(event) {
   event.preventDefault();
   c++;
@@ -15,7 +16,7 @@ function handleFormSubmit(event) {
   }
  
   li.id = `${c}`;
-  li.innerHTML =  `${username} + ${email} + ${phone} ` + `<button onclick = "de(event)" id = ${c} > Delete </button>`;
+  li.innerHTML =  `${username} + ${email} + ${phone} ` + `<button onclick = "de(event)" id = ${c} > Delete </button><t><button onclick = "del(event)" id = ${c} > Edit </button>` ;
   localStorage.setItem(`User Details ${c}`,JSON.stringify(obj));
   adding(li);
 }
@@ -26,6 +27,20 @@ function de(event) {
     if(event.target.id === lis[i].id){
       ul.removeChild(lis[i]);
       localStorage.removeItem(`User Details ${lis[i].id}`);
+    }
+  } 
+}
+// edit
+function del(event) {
+  let lis = document.querySelectorAll("li");
+  for (let i = 0; i < lis.length; i++) {
+    if(event.target.id === lis[i].id){
+      ul.removeChild(lis[i]);
+      document.getElementById("username").value = JSON.parse(localStorage.getItem(`User Details ${lis[i].id}`)).username;
+      document.getElementById("email").value = JSON.parse(localStorage.getItem(`User Details ${lis[i].id}`)).email;
+      document.getElementById("phone").value = JSON.parse(localStorage.getItem(`User Details ${lis[i].id}`)).phone;
+      localStorage.removeItem(`User Details ${lis[i].id}`);
+  
     }
   } 
 }
