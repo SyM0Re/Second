f8c0a460cd524f7166481b005f4a090f80b369ec
SyM0Re <smrrehansmr@gmail.com>
2024-02-01 20:43:53 +0530

    Promises (async /await)

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-let c = 0; 
-let ul = document.querySelector("ul");
-function handleFormSubmit(event){
-    c++;
-    event.preventDefault();
-    let li = document.createElement("li");
-    let ExpAmount = event.target.exp.value;
-    let ExpDes = event.target.Des.value;
-    let ExpCat = event.target.Cat.value;
-    
-    let Expense = {
-        ExpAmount : ExpAmount,
-        ExpDes : ExpDes,
-        ExpCat : ExpCat
-    };
-    localStorage.setItem(`Expense ${c}`, JSON.stringify(Expense));
-    li.innerHTML = ` ${ExpAmount} - ${ExpDes} - ${ExpCat} ` + `<button onclick = "de(event , ${c})"> Delete </button><button onclick = "ed(event , ${c})"> Edit </button>`;
-    li.id = `${c}`;
-    ul.appendChild(li);
+
+let tic = async () => {
+    let ticket = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("ticket"),3000);
+    });
+
+    let getPopcorn = new Promise((resolve, reject) => resolve("Popcorn"));
+    let getButter = new Promise((resolve, reject) => resolve("Butter"));
+    let getColdDrinks = new Promise((resolve, reject) => resolve("Cold Drink"));
+
+    let [Popcorn,Butter,ColdDrink] = await Promise.all([getPopcorn,getButter,getColdDrinks]);
+    console.log(`${Popcorn}, ${Butter}, ${ColdDrink}`);
+    return ticket;
 }
-function de(event , c) {
-    localStorage.removeItem(`Expense ${c}`);
-    let l = document.getElementById(`${c}`);
-    ul.removeChild(l);
-}
-function ed(event , c) {
-    let r = JSON.parse(localStorage.getItem(`Expense ${c}`));
-    document.getElementById("exp").value = r.ExpAmount; 
-    document.getElementById("Des").value = r.ExpDes;
-    document.getElementById("Cat").value = r.ExpCat;
-    localStorage.removeItem(`Expense ${c}`);
-    let l = document.getElementById(`${c}`);
-    ul.removeChild(l);
-}
+console.log(tic());
